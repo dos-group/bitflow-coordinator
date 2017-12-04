60d6d2229685d5eaf85c62f6bc3d5b8de17b2fe1
Christian <christian@wlan-141-23-158-142.tubit.tu-berlin.de>
2017-12-04 11:35:20 +0100

    remove password

--- src/models/user.js
+++ src/models/user.js
@@ -6,7 +6,6 @@ let userSchema = new Schema({
   id: { type: Number, integer: true },
   name: { type: String, required: true },
   email: { type: String, required: true },
-  password: { type: String, required: true },
   registeredSince: { type: String, required: true }
 });
 
