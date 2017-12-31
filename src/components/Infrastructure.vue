dd655c64e69fe6dc982aee39848df59730573d6f
Christian <christian@ChristiansMBP2.home>
2017-12-31 18:29:54 +0100

    start implementing infrastrcutre view

--- src/components/Infrastructure.vue
+++ src/components/Infrastructure.vue
@@ -1,14 +1,52 @@
 <template>
   <div class="page-content">
-  	<h1>Infrastructure</h1>
+		<h1>{{ title }}</h1>
+		<div class="top-content">
+			<b-card-group deck>
+				<b-card :title="numberOfAgents">
+					<p class="card-text">Active Agents</p>
+				</b-card>
+				<b-card :title="numberOfIdleAgents">
+					<p class="card-text">Idle Agents</p>
+				</b-card>
+			</b-card-group>
+		</div>
+		<b-table outlined small stacked :items="agents"/>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
 	name: 'Infrastructure',
 	data() {
-		return {}
+		return {
+			title: "Infrastructure",
+			numberOfAgents: null,
+			numberOfIdleAgents: null,
+			agents: []
+		};
+	},
+	async created() {
+		try {
+			const response = await axios.get(this.$baseUrl + "/info");
+			const info = response.data[0];
+			this.numberOfAgents = String(info.numberOfAgents);
+			this.numberOfIdleAgents = String(info.numberOfIdleAgents);
+			this.agents = info.agents;
+		} catch (e) {
+			alert(e);
+		}
 	}
 }
 </script>
+
+<style scoped>
+.top-content {
+	padding: 30px 0px 40px 0px;
+}
+.card-text {
+	font-size: 20px;
+}
+</style>
