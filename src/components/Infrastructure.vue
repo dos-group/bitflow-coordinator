e5d197164c505eef5bb4bc359e63ee8f83ff7149
Prateek Narula <prateeknarula@prateeks-mbp.localdomain>
2018-01-09 00:19:41 +0100

    Infrasturcture Vieew changed

--- src/components/Infrastructure.vue
+++ src/components/Infrastructure.vue
@@ -1,54 +1,75 @@
 <template>
   <div class="page-content">
 		<h1>{{ title }}</h1>
-		<div class="top-content">
-			<b-card-group deck>
-				<b-card :title="numberOfAgents">
-					<p class="card-text">Active Agents</p>
-				</b-card>
-				<b-card :title="numberOfIdleAgents">
-					<p class="card-text">Idle Agents</p>
-				</b-card>
-			</b-card-group>
-		</div>
-		<b-tabs>
-			<b-tab title="Agents" active>
-				<b-table striped responsive :items="agents" :fields="agentFields">
-					<template slot="tags" slot-scope="row">
-						<b-button size="sm" @click.stop="row.toggleDetails" class="mr-2">
-						{{ row.detailsShowing ? 'Hide' : 'Show'}} Tags
-						</b-button>
-					</template>
-					<template slot="row-details" slot-scope="row">
-						<b-card>
-							<b-row v-for="tag in row.item.tags" :key="tag.slots" class="mb-2">
-								<b-col sm="2" class="text-sm-right"><b>Resources:</b></b-col>
-								<b-col>{{ tag.resources }}</b-col>
-								<b-col sm="2" class="text-sm-right"><b>Slots:</b></b-col>
-								<b-col>{{ tag.slots }}</b-col>
-							</b-row>
+		<b-container class="bv-example-row">
+			 <b-row>
+		        <b-col cols="3">
+						<b-card :title="numberOfAgents">
+							<p class="card-text">Active Agents</p>
 						</b-card>
-					</template>
-					<template slot="usedCpuCores" slot-scope="data">
-						{{ data.item.usedCpuCores.join(', ') }}
-					</template>
-				</b-table>
-			</b-tab>
-			<b-tab title="Pipelines">
-				<ul class="pipeline-list list-group">
-					<li v-for="item in pipelines" :key="item.id">
-						<div class="list-item list-group-item">
-							<b-btn class="btn btn-outline-success btn-md float-right action-button">
-								Start Pipeline
-							</b-btn>
-							<div class="pipeline-box">
-								<h5>{{ item.name }}</h5>
-							</div>
-						</div>
-					</li>
-				</ul>
-			</b-tab>
-		</b-tabs>
+						<b-card :title="numberOfIdleAgents">
+							<p class="card-text">Idle Agents</p>
+						</b-card>	
+				</b-col>
+		        <b-col cols="9">
+		        	<b-tabs>
+						<b-tab title=" Active Agents" active>
+							<b-table striped responsive :items="agents" :fields="agentFields">
+								<template slot="tags" slot-scope="row">
+									<b-button size="sm" @click.stop="row.toggleDetails" class="mr-2">
+									{{ row.detailsShowing ? 'Hide' : 'Show'}} Tags
+									</b-button>
+								</template>
+								<template slot="row-details" slot-scope="row">
+									<b-card>
+										<b-row v-for="tag in row.item.tags" :key="tag.slots" class="mb-2">
+											<b-col sm="2" class="text-sm-right"><b>Resources:</b></b-col>
+											<b-col>{{ tag.resources }}</b-col>
+											<b-col sm="2" class="text-sm-right"><b>Slots:</b></b-col>
+											<b-col>{{ tag.slots }}</b-col>
+										</b-row>
+									</b-card>
+								</template>
+								<template slot="usedCpuCores" slot-scope="data">
+									{{ data.item.usedCpuCores.join(', ') }}
+								</template>
+							</b-table>
+						</b-tab>
+						<b-tab title="Pipelines">
+							<ul class="pipeline-list list-group">
+								<b-card-group columns>
+								<li v-for="item in pipelines" :key="item.id">
+									<b-card>
+										<p>{{ item.name }}</p>
+										<b-btn class="btn btn-outline-success float-left action-button">
+											Start 
+										</b-btn>
+										<b-btn class="btn btn-outline-success action-button">
+											Details
+										</b-btn>
+									</b-card>
+								</li>
+								</b-card-group columns>
+							</ul>
+						</b-tab>
+					</b-tabs>
+		        </b-col>
+		    </b-row>
+		    <b-row>
+		    <div>
+		    		<div class="pipeline-box">
+						<h5>testing</h5>
+					</div>
+		    </div>
+		    </b-row>
+
+		</b-container>
+
+		<div class="top-content">
+			
+			
+		</div>
+		
   </div>
 </template>
 
