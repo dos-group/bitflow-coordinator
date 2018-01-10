f2d8e2accc03aa9eced787ceafe9fb0eb22c3b05
Ankita Shah Ashwin <ankita.shah1301@campus.tu-berlin.de>
2018-01-10 13:28:12 +0100

    Corrections made.

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,19 +1,17 @@
 <template>
-    <div class="form-group row ">
-        <div class="col-xs-2 btn-group">
-            <b-card>
-        <b-form @submit="onSubmit" v-if="show" >
-            <b-form-group id="emailaddress" label="Email address:"label-for="emailaddress" >
-            <b-form-input id="emailaddress" type="email" v-model="form.email" required placeholder="Enter email">
-            </b-form-input>
-            </b-form-group>
-            <b-form-group id="password" label="Your Name:" label-for="password">
+    <div class="container row">
+        <b-card>
+            <b-form @submit="onSubmit" v-if="show">
+                <b-form-group id="emailaddress" label="Email address:" label-for="emailaddress" >
+                <b-form-input id="emailaddress" type="email" v-model="form.email" required placeholder="Enter email">
+                </b-form-input>
+                </b-form-group>
+                <b-form-group id="password" label="Your Name:" label-for="password">
                 <b-form-input id="password" type="password" v-model="form.password" required placeholder="Enter password"></b-form-input>
-            </b-form-group>
-            <b-button type="submit" variant="primary">Login</b-button>
-        </b-form>
-            </b-card>
-        </div>
+                </b-form-group>
+                <b-button type="submit" variant="primary">Login</b-button>
+                </b-form>
+        </b-card>
     </div>
 </template>
 
@@ -31,9 +29,15 @@
         methods: {
             onSubmit (evt) {
                 evt.preventDefault();
-//                alert("Hello");
                 this.$router.push('projects');
             },
         }
     }
 </script>
+<style>
+    .container {
+        width: 2000px;
+        margin: 50px;
+        margin-left: 750px;
+    }
+</style>
