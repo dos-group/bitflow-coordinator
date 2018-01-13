5ddf7a024875f5acc70d17ac19c1179d93baf301
Christoph Portmann <christoph.portmann@goeuro.com>
2018-01-13 13:56:11 +0100

    reformat code with indent 2

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,20 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Login from '@/components/Login'
-import Projects from '@/components/Projects'
-import Infrastructure from '@/components/Infrastructure'
-import Pipelines from '@/components/Pipelines'
-import Editor from '@/components/Editor'
+import Vue from "vue";
+import Router from "vue-router";
+import Login from "@/components/Login";
+import Projects from "@/components/Projects";
+import Infrastructure from "@/components/Infrastructure";
+import Pipelines from "@/components/Pipelines";
+import Editor from "@/components/Editor";
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-  	{
-  		path: '/',
-  		name: 'Login',
-  		component: Login
-  	},
+    {
+      path: '/',
+      name: 'Login',
+      component: Login
+    },
     {
       path: '/projects',
       name: 'Projects',
@@ -26,14 +26,14 @@ export default new Router({
       component: Infrastructure
     },
     {
-     	path: '/projects/:project_id/pipelines',
+      path: '/projects/:project_id/pipelines',
       name: 'Pipelines',
       component: Pipelines
     },
     {
-    	path: '/projects/:project_id/pipelines/:pipeline_id/editor',
-    	name: 'Editor',
-    	component: Editor
+      path: '/projects/:project_id/pipelines/:pipeline_id/editor',
+      name: 'Editor',
+      component: Editor
     }
   ],
   mode: "history"
